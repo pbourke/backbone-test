ba09333e9fbbce20f0d0d2433c563813d8051fdb
Patrick Bourke <pbourke@gmail.com>
2012-08-28 10:57:15 -0700

    working sample app

--- new-deps/js/app.js
+++ new-deps/js/app.js
@@ -28,13 +28,25 @@ $(document).ready(function() {
    });
 
    NameView = Backbone.View.extend({
+      tagName : "li",
+
       initialize: function() {
          this.model.on("all", this.render, this);
       },
 
+      events: {
+         "click .removeName": "removeName"
+      },
+
       render: function() {
-         $(this.el).html(ich.nameRow(this.model.toJSON()).html());
+         this.$el.html(ich.nameRow(this.model, true));
          return this;
+      },
+
+      removeName: function(evt) {
+         evt.preventDefault();
+         this.model.collection.remove(this.model);
+         this.remove();
       }
    });
 
@@ -52,6 +64,53 @@ $(document).ready(function() {
       }
    });
 
+   SummaryView = Backbone.View.extend({
+      initialize: function() {
+         this.collection.on("all", this.render, this)
+      },
+
+      render: function(e) {
+         var summary = ich.summaryView({ 
+            count: this.collection.length,
+            empty: this.collection.length === 0
+         });
+         this.$el.children().remove();
+         this.$el.append(summary);
+         return this;
+      }
+   });
+
+   LetterSummaryView = Backbone.View.extend({
+      initialize: function() {
+         this.collection.on("all", this.render, this)
+      },
+
+      render: function(e) {
+         ltrLst = _.map(this.collection.models.map( function(name) {
+                      // list of names -> list of first letters
+                      return name.id[0];
+                   }).reduce( function(m, letter) {
+                      // list of letters -> map of letter to count of letters
+                      m[letter] = _.isUndefined(m[letter]) ? 1 : m[letter] + 1;
+                      return m;
+                   }, {}), function(v, k){
+                      // map of letter->count to [{letter:'a', count:1}, ...]
+                      return {
+                        letter:k, 
+                        count:v
+                      };
+                   });
+         console.log(ltrLst);
+         var summary = ich.letterSummaryView({ 
+            letters: ltrLst
+         }, true);
+         console.log(summary);
+         this.$el.children().remove();
+         this.$el.append(summary);
+         return this;
+      }
+   });
+
    window.nameCollection = new NameCollection();
 
    new AddFormView({
@@ -63,6 +122,16 @@ $(document).ready(function() {
       "el": $("#names"),
        "collection": nameCollection
    });
+
+   new SummaryView({
+      "el": $("#summaryViewContainer"),
+      "collection": nameCollection
+   }).render();
+
+   new LetterSummaryView({
+      "el": $("#letterSummaryViewContainer"),
+      "collection": nameCollection
+   }).render();
 
    $(".tooltipEnabled").tooltip();
 });
